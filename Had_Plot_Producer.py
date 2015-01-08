1c0475759a4b0dede203d82e40b596b19dcc0b9e
Matthew <mcitron@cern.ch>
2015-01-08 13:15:42 +0000

    set up for PF

--- Had_Plot_Producer.py
+++ Had_Plot_Producer.py
@@ -46,7 +46,7 @@ trigger_effs = {"200_1":0.68,"200_2":0.62, "200_3":0.48, "200_4":0.48, #200_4 is
 settings = {
         "dirs":["200_275","275_325","325_375","375_475","475_575","575_675","675_775","775_875","875_975","975_1075","1075"],
   # "Plots":["MHTovMET_all", "MET_all","MHT_all","AlphaT_all","JetMultiplicity_all","HT_all","Number_Btags_all","CommonJetPt_all","CommonJetEta_all","Number_Good_verticies_all"][4:5],
-  "Plots":["MET_all", "MHT_all", "ComMinBiasDPhi_acceptedJets_all"][3:],
+  "Plots":["MET_all", "MHT_all", "ComMinBiasDPhi_acceptedJets_all"],
   "Lumo" : this_run()["had_lumi"]*10.,
   "Webpage":"btag",
   "Category":"Had",
